25c6bded63e06cf41399663340c2c9fb51350cc6
shivam585 <85876404+shivam585@users.noreply.github.com>
2021-10-30 08:59:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+
+function preload()
+{
+ 
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+ 
+g=createSprite(400,400,20,20)
+ 
+ }
+
+function draw() {
+  
+ 
+
+  drawSprites();
+ 
+}
+
